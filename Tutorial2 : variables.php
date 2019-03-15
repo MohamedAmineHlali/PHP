0931ae2c24ddb1531455fba241697f2340d8af4b
MohamedAmineHlali <48563200+MohamedAmineHlali@users.noreply.github.com>
2019-03-15 16:11:16 +0100

    Tutorial2 : variables.php

---
+++ Tutorial2 : variables.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+arithmetic operators
+*/ 
+
+//variables
+
+$varA  = 10 ;
+$varB  = 5 ;
+$varC  = $varA + $varB ;
+$varD  = $varA - $varB ;
+
+//addition [+]
+echo  $varC ; echo "<br>";
+//substraction [-]
+echo $varD ; echo "<br>";
+//Multiplication [*]
+echo $varA * $varB ; echo "<br>";
+//Division [/]
+echo $varA / $varB ; echo "<br>";
+
+
+?>
